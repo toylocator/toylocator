23592f5b2c0a6a2cfe0a0087271d6e83df945c76
Taeil Goh <taeil.goh@berkeley.edu>
2020-11-01 09:51:58 -0800

    restructured folders structure to match cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deep Learning application to locate a toy at ha household',
+    author='UC Berkeley',
+    license='BSD-3',
+)
