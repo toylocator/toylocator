74f1ebd965a90802ec52a20b99a3c111914f5758
CHENLIN YE <CHENLINY@BERKELEY.EDU>
2020-11-01 14:39:43 -0800

    added user input and revised output directory

--- src/autolabel/cameraFrame.py
+++ src/autolabel/cameraFrame.py
@@ -1,18 +1,27 @@
-import numpy as np
+#!usr/bin/python
+
 import cv2 as cv
 import os
+import sys
 
 cap = cv.VideoCapture(0)
 cap.set(cv.CAP_PROP_FRAME_WIDTH, 680)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 680)
 
 # placeholder for object class
-object = 'defaultClass'
+if len(sys.argv) == 1:
+    print("Please enter the name for the object.")
+    exit()
+
+object = str(sys.argv[1])
 
 # create data folder for output frames
-output_path = './rawImages'
+output_path = '../../data/raw/{}/'.format(object)
 if not os.path.exists(output_path):
     os.makedirs(output_path)
+else:
+    print("An obejct with the same name exists; please use a different name and try again:)")
+    exit()
 
 # Preparation countdown
 countdown = 110
