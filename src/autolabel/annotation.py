26d4678188a187f7f44a380b94669fa9726df173
Hongsuk Nam <46408942+aquantum1@users.noreply.github.com>
2020-11-10 23:36:00 -0800

    Add files via upload

---
+++ src/autolabel/annotation.py
@@ -0,0 +1,159 @@
+import glob, shutil
+import os
+import pickle
+import xml.etree.ElementTree as ET
+from os import listdir, getcwd
+from os.path import join
+import numpy as np
+import sys
+
+"""
+Reads images from raw data directory and prepares Yolo structure.
+"""
+
+input_path = '../../data/raw/'
+output = '../../data/processed/'
+
+# Read in latest class label from latest_label.txt
+txt_file_path = input_path + 'latest_label.txt'
+with open(txt_file_path, 'r') as file:
+    obj = file.read()
+
+# Append class label to existing inventory
+inventory_path = output + 'label_inventory.txt'
+
+# Create yolo folder structure - images folder
+train_img_output_path = output + 'train/images/'
+validate_img_output_path = output + 'validate/images/'
+
+img_dir = [train_img_output_path, validate_img_output_path]
+for dir in img_dir:
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+# Check if class label exists in inventory already
+# If exits, inform and exit
+# If not, proceed with appending the class label
+try:
+    with open(inventory_path, 'r+') as file:
+        temp_set = set(file.read().splitlines())
+        if obj in temp_set:
+            print("We had annotated this object already:)")
+        # If class label does not exit in inventory list, append to list
+        else:
+            file.write(obj + '\n')
+
+# If inventory list does not exist, create one and append class label
+except:
+    with open(inventory_path, 'a') as file:
+        file.write(obj + '\n')
+
+# Read in inventory list to classes
+with open(inventory_path, 'r') as file:
+    classes = file.read().splitlines()
+
+# raw image outputs from camera; input for auto-labeling
+rawImage_dir = '../../data/raw/{}/'.format(obj)
+
+dirs = ['train', 'validate']
+
+# Function to obtain a list of .jpg images in directory
+def getImagesInDir(dir_path):
+    """
+    Function to obtain a list of .jpg files in a directory.
+    Parameters:
+        - dir_path: directory for the training images (from camera output)
+    """
+    image_list = []
+
+    for filename in glob.glob(dir_path + '/*.jpg'):
+        image_list.append(filename)
+    return image_list
+
+def datasetSplit(img_lst):
+    """
+    Function to split the image_list to training/validation sets.
+    Parameters:
+        - img_list: list of images
+    """
+    num = len(img_lst)
+
+    idx = np.random.permutation(num)
+    train_lst = np.array(img_lst)[idx[:int(num * .8)]]   # 80/20 split
+    validation_lst = np.array(img_lst)[idx[:int(num * .2)]]
+    return train_lst, validation_lst
+
+def convert(size=(640, 480)):
+    """
+    Function to generate YOLO bbox parameters. The dimensions are hard-coded
+    since our bounding box is fixed. We won't need this function in production.
+    parameters:
+        - size: tuple containing width and height of raw image
+    """
+    dw = 1./(size[0])   # 1 / image width
+    dh = 1./(size[1])   # 1 / image height
+
+    
+    # Read in bbox coordinate information from bbox_information.txt
+    bbox_path = input_path + 'bbox_information.txt'
+    with open(bbox_path, 'r') as file:
+        for num, line in enumerate(file):
+            if num == i:
+                x = int(line.split()[0])
+                y = int(line.split()[1])
+                w = int(line.split()[2])
+                h = int(line.split()[3])
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+
+    return (x,y,w,h)
+
+
+def annotate(output_path, image_path):
+    """
+    Main function to create the annotation .txt files.
+    parameters:
+        - dir_path: directory of training or validation images
+        - output_path: destination for annotated .txt files
+        - image_path: directory to individual image
+    """
+    basename = os.path.basename(image_path)  # extract file name only
+    basename_no_ext = os.path.splitext(basename)[0]   # extract file name without extension
+
+    out_file = open(output_path + basename_no_ext + '.txt', 'w')   # write .txt file with same file name
+    bb = convert()
+    cls = obj
+    cls_id = classes.index(cls)  # [TODO] This classes will be read in from .txt file at beginning
+    out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+
+# Execution
+image_paths = getImagesInDir(rawImage_dir)
+train_paths, validation_paths = datasetSplit(image_paths)
+image_sets = [train_paths, validation_paths]
+
+for i, image_paths in enumerate(image_sets):
+
+    # output path to be either {$PWD}/train or {$PWD}/validate
+    full_dir_path = output + dirs[i]
+
+    # output path in the labels folder
+    output_path = full_dir_path + '/labels/'
+
+    # create label directory if not exists
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    # copy train/validation images to images folder
+    for file in image_paths:
+        shutil.copy(file, full_dir_path + '/images')
+
+    # generate annotation files to labels folder
+    for path in image_paths:
+        annotate(output_path, path)
+
+num_train = len(train_paths)
+num_validate = len(validation_paths)
+# print("Processed {} training and {} validation".format(num_train, num_validate))
+print("Process completed")
---
+++ src/autolabel/object_track.py
@@ -0,0 +1,88 @@
+import cv2 as cv
+import os
+import sys
+import time
+
+# placeholder for object class
+if len(sys.argv) < 2:
+    print("Please enter the name for the object.")
+    exit()
+
+source = 0
+if len(sys.argv) == 3:
+    source = str(sys.argv[2])
+
+obj = str(sys.argv[1])
+
+cap = cv.VideoCapture(source)
+time.sleep(5)
+cap.set(3, 640)
+cap.set(4, 480)
+
+# directry to output latest_label.txt file and raw images 
+PATH = '../../data/raw/'
+
+# create data folder for output frames
+output_path = PATH + obj
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+else:
+    print("An obejct with the same name exists; please use a different name and try again:)")
+    exit()
+
+# write the class name to latest_label.txt
+txt_file_path = PATH + 'latest_label.txt'
+with open(txt_file_path, 'w') as file:
+    file.write(obj)
+
+# tracker = cv.TrackerMOSSE_create()
+tracker = cv.TrackerCSRT_create()
+# tracker = cv.TrackerGOTURN_create()
+# read initial frame for customized bounding box
+ret, frame = cap.read()
+bbox = cv.selectROI('Tracking', frame, False)
+initial_txt = 'Please draw a bounding box'
+cv.putText(frame, initial_txt, (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv.LINE_AA)
+
+# initialzie tracker using the bounding box
+tracker.init(frame, bbox)
+
+def drawBbox(frame, bbox):
+    """
+    A function that draws boudning box on a frame based on the bbox dimensions.
+    """
+    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+    cv.rectangle(frame, (x, y), ((x + w), (y+h)), (255, 0, 0), 2, 1)
+
+i = 0
+while True:
+    # read frame
+    ret, frame = cap.read()
+    # get bbox and updates tracker
+    ret, bbox = tracker.update(frame)
+    if ret:
+        # draw bbox if traking succeeded
+        drawBbox(frame, bbox)
+    else:
+        # print missing if not
+        cv.putText(frame, 'lost', (100, 145), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv.LINE_AA)
+    # display status
+    txt = 'Capturing training samples'
+    cv.putText(frame, txt, (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv.LINE_AA)
+    
+    ##### ADD IMAGE CAPTURE and annotation CODE HERE ######
+    #######################################################
+    cv.imwrite(output_path+"/"+obj+"_"+str(i)+".jpg",frame)
+    i += 1    
+    
+    # append the bbox coordinate to bbox_information.txt
+    bbox_coordinate = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+    bbox_path = PATH + 'bbox_information.txt'
+    with open(bbox_path, 'a') as file:
+        file.write(" ".join([str(a) for a in bbox_coordinate]) + '\n')
+
+
+    #######################################################
+    cv.imshow('tracking', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
