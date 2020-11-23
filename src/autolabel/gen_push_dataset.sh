502bc3b0a6472aa4622bc6439ef8ad091cb40034
taeil <taeil.goh@berkeley.edu>
2020-11-22 21:13:20 -0800

    added shell script to combine script runs

--- src/autolabel/gen_push_dataset.sh
+++ src/autolabel/gen_push_dataset.sh
@@ -4,12 +4,16 @@
 rm -rf ../../data/augmented/
 rm -rf ../../data/processed/
 
+# rotate shift scale noise are optional but at least one of them needs to be specified 
 python3 augmentation.py rotate shift scale flip 
 
+# download the latest label inventory 
 aws s3 cp s3://toylocator/data/label_inventory.txt ../../data/label_inventory.txt
 
+# convert annotation format
 python3 aug_annotation.py
 
+# upload dataset to repository 
 aws s3 cp ../../data/processed/train s3://toylocator/data/train --recursive
 aws s3 cp ../../data/processed/validate s3://toylocator/data/validate --recursive
 aws s3 cp ../../data/label_inventory.txt s3://toylocator/data/label_inventory.txt 
