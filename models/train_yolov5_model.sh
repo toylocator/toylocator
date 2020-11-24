2da50d32236194d5537f5095e2a295a479de1e9f
Taeil Goh <taeil.goh@berkeley.edu>
2020-11-23 21:51:46 -0800

    updated shell scripts on cloud training

--- models/train_yolov5_model.sh
+++ models/train_yolov5_model.sh
@@ -9,8 +9,10 @@ cp -f runs/exp0_yolov5s_results/weights/best.pt /data
 
 python3 detect.py --weights /data/best.pt --img 416 --conf 0.4 --source /data/5_toys.v2.yolov5pytorch/test/images
 
-jupyter lab --ip=0.0.0.0 --no-browser
-
 # aws s3 cp s3://toylocator/model/best.pt .
-# aws s3 cp runs/exp0_yolov5s_results/weights/last.pt s3://toylocator/model/1122/last.pt 
-# aws s3 cp <any log to keep> s3://toylocator/model/1122 --recursive 
+
+model_dir=$(date +'%m-%d-%Y-%0l%p')
+aws s3 cp runs/exp0_yolov5s_results/weights/last.pt s3://toylocator/model/last.pt 
+aws s3 cp runs/exp0_yolov5s_results s3://toylocator/model/$model_dir --recursive 
+
+#jupyter lab --ip=0.0.0.0 --no-browser
