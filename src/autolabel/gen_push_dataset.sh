008299d81f8b32d663d2dc65c1691710bfee058a
taeil <taeil.goh@berkeley.edu>
2020-11-26 19:46:15 -0800

    handling out of bound annotation

--- src/autolabel/gen_push_dataset.sh
+++ src/autolabel/gen_push_dataset.sh
@@ -5,13 +5,13 @@ rm -rf /data/augmented
 rm -rf /data/processed
 
 # rotate shift scale noise are optional but at least one of them needs to be specified 
-python3 augmentation.py rotate shift flip noise  
+python3 src/autolabel/augmentation.py rotate shift flip noise  
 
 # download the latest label inventory 
-aws s3 cp s3://toylocator/data/label_inventory.txt ../../data/label_inventory.txt
+aws s3 cp s3://toylocator/data/label_inventory.txt /data/label_inventory.txt
 
 # convert annotation format
-python3 aug_annotation.py
+python3 src/autolabel/annotation.py
 
 # upload dataset to repository 
 aws s3 cp /data/processed/train s3://toylocator/data/train --recursive
