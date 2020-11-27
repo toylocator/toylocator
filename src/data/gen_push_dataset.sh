08d458e1ed150fdb686960f0966d250a2d65a1e8
Taeil Goh <taeil.goh@berkeley.edu>
2020-11-27 10:24:16 -0800

    fixed script to upload test data set to s3

--- src/data/gen_push_dataset.sh
+++ src/data/gen_push_dataset.sh
@@ -16,5 +16,6 @@ python3 src/data/annotation.py
 # upload dataset to repository
 aws s3 cp /data/processed/train s3://toylocator/data/train --recursive
 aws s3 cp /data/processed/validate s3://toylocator/data/validate --recursive
+aws s3 cp /data/processed/test s3://toylocator/data/test --recursive
 aws s3 cp /data/processed/error s3://toylocator/error/images --recursive
 aws s3 cp /data/label_inventory.txt s3://toylocator/data/label_inventory.txt
