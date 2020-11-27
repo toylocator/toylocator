4e5b8846ea6b11df98075503ee2fa44f6e69c748
taeil <taeil.goh@berkeley.edu>
2020-11-26 19:50:25 -0800

    handling out of bound annotation

--- src/autolabel/gen_push_dataset.sh
+++ src/autolabel/gen_push_dataset.sh
@@ -13,8 +13,8 @@ aws s3 cp s3://toylocator/data/label_inventory.txt /data/label_inventory.txt
 # convert annotation format
 python3 src/autolabel/annotation.py
 
-# upload dataset to repository 
+# upload dataset to repository
 aws s3 cp /data/processed/train s3://toylocator/data/train --recursive
 aws s3 cp /data/processed/validate s3://toylocator/data/validate --recursive
-aws s3 cp /data/label_inventory.txt s3://toylocator/data/label_inventory.txt 
-
+aws s3 cp /data/processed/error s3://toylocator/error/images --recursive
+aws s3 cp /data/label_inventory.txt s3://toylocator/data/label_inventory.txt
