4461a75a2d12f13295ae900b1a9edf7fb4fd47b8
Taeil Goh <taeil.goh@berkeley.edu>
2020-11-29 17:59:54 -0800

    updated training code

--- src/models/train_yolov5_model.sh
+++ src/models/train_yolov5_model.sh
@@ -13,18 +13,18 @@ rm /data/*.template
 # generate data.yaml and custom_yolov5s.yaml
 python3 gen_yolov5_yaml.py
 
-epoch = 200
+epoch = 300
 batch = 16
-yolov5_pt = yolov5s
+yolov5_pt = yolov5m
 python3 train.py --img-size 1920 --rect --batch $batch --epochs $epoch --data '/data/data.yaml' --cfg /data/custom_yolov5s.yaml --weights ${yolov5_pt}.pt --name ${nc}cls_${epoch}epcs_${yolov5_pt} --cache
-
-model_dir=$(date +'%m-%d-%Y-%0l%p')
-aws s3 cp runs/train/${nc}cls_${epoch}epcs_${yolov5_pt} s3://toylocator/model/${nc}cls_${epoch}epcs_${yolov5_pt}/$model_dir --recursive
 
 # upload the model
 model_dir=$(date +'%m-%d-%Y')
 aws s3 cp runs/train/${nc}cls_${epoch}epcs_${yolov5_pt}/weights/last.pt s3://toylocator/model/last.pt
-aws s3 cp runs/train/${nc}cls_${epoch}epcs_${yolov5_pt} s3://toylocator/model/$model_dir --recursive
+aws s3 cp runs/train/${nc}cls_${epoch}epcs_${yolov5_pt} s3://toylocator/model/${nc}cls_${epoch}epcs_${yolov5_pt}/$model_dir --recursive
+
+# Test the model
+python3 test.py --img-size 1920 --batch $batch --data '/data/data.yaml' --weights runs/train/${nc}cls_${epoch}epcs_${yolov5_pt}/weights/last.pt --name ${nc}cls_${epoch}epcs_${yolov5_pt} --iou-thres ${iou} --task test --cache
 
 # Run inference on test images (optional)
 # python3 detect.py --weights runs/train/${nc}cls_${epoch}epcs_${yolov5_pt}/weights/last.pt --img-size 1920 --conf 0.4 --source /data/test/images
