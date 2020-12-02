a7bd5745c82e0b994db1d62a9d997e961edef63b
Taeil Goh <taeil.goh@berkeley.edu>
2020-12-01 23:14:12 -0800

    training versioning

---
+++ reports/README.md
@@ -0,0 +1,18 @@
+
+
+#### v1 high resolution 
+
+Training Data
+- 2 ~ 8 classes 
+- 1920 x 1080 resolutions 
+- s3://toylocator/data/video/train
+- model converge and high metrics with validation/test data but not on production inference
+
+
+#### v2 [low resolution : 640](https://wandb.ai/taeil/toydetector/runs)
+ 
+2 clases 
+- 
+ 
+[9 classes 100epcs yolov5s](https://wandb.ai/taeil/toydetector/runs/2kxojrqv/overview?workspace=user-taeil)
+- 9cls_100epcs_5s
--- src/models/train_yolov5_model.sh
+++ src/models/train_yolov5_model.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+version=$1
 # Copy data from S3 bucket
 
 # training only 2 classes
 # aws s3 sync s3://toylocator/data_5cls_bk /data --exclude "video/*"
-aws s3 sync s3://toylocator/data /data --exclude "video/*"
+aws s3 sync s3://toylocator/data/${version} /data --exclude "video/*"
 
 nc=$(cat /data/label_inventory.txt | wc -l)
 mv /data/custom_yolov5s.yaml /data/custom_yolov5s.template
@@ -25,15 +26,15 @@ python3 gen_yolov5_yaml.py
 # python3 train.py --img-size 1920 --rect --batch 16 --epochs 1 --data '/data/data.yaml' --cfg /data/custom_yolov5s.yaml --weights yolov5s.pt --name smoke_24_1epcs --cache
 
 # full training
-epoch=200
-batch=64
-yolov5_pt=5s
-python3 train.py --img 640 --batch $batch --epochs $epoch --data '/data/data.yaml' --cfg /data/custom_yolov5s.yaml --weights yolov${yolov5_pt}.pt --name ${nc}cls_${epoch}epcs_${yolov5_pt} --cache --log-imgs 100 --project toydetector
+epoch=$2
+batch=$3
+yolov5_pt=$4
+python3 train.py --img 640 --batch $batch --epochs $epoch --data '/data/data.yaml' --cfg /data/custom_yolov5s.yaml --weights yolov${yolov5_pt}.pt --name ${nc}cls_${epoch}epcs_${yolov5_pt} --cache --log-imgs 100 --project ${version}
 
 # upload the model
 model_dir=$(date +'%m-%d-%Y')
 aws s3 cp runs/train/${nc}cls_${epoch}epcs_${yolov5_pt}/weights/best.pt s3://toylocator/model/best.pt
-aws s3 cp runs/train/${nc}cls_${epoch}epcs_${yolov5_pt} s3://toylocator/model/${nc}cls_${epoch}epcs_${yolov5_pt}/$model_dir --recursive
+aws s3 cp runs/train/${nc}cls_${epoch}epcs_${yolov5_pt} s3://toylocator/model/${version}/${nc}cls_${epoch}epcs_${yolov5_pt}/$model_dir --recursive
 
 # Test the model
 # iou=0.4
